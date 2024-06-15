e08b90e867f1bb9b864b832b652886553d3f4fdb
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-06-15 22:06:41 +0800

    WIP ctor

--- src/deque_unique.h
+++ src/deque_unique.h
@@ -1,7 +1,81 @@
 template <typename T, typename Hash>
 
-class DequeOfUniqueEntries {
- public:
+#pragma once
 
-  VectorOfUniqueEntries() = default;
-}
+#include <deque>
+#include <initializer_list>
+#include <stdexcept>
+#include <unordered_set>
+#include <utility>
+
+namespace uniquecontainers {
+
+// Define hash for std::pair<T1, T2>
+struct pair_hash {
+  template <class T1, class T2>
+  std::size_t operator()(const std::pair<T1, T2>& p) const {
+    auto hash1 = std::hash<T1>{}(p.first);
+    auto hash2 = std::hash<T2>{}(p.second);
+    return hash1 ^ (hash2 << 1);
+  }
+};
+
+template <typename T1, typename T2>
+class DequeOfUniquePairs {
+ public:
+   using value_type = T;
+
+  DequeOfUniquePairs() = default;
+
+  DequeOfUniquePairs(const std::initializer_list<std::pair<T1, T2>>& initializer):DequeOfUniquePairs(initializer.begin(), initializer.end()){};
+
+  DequeOfUniquePairs(const DequeOfUniquePairs& other)=default;
+
+  DequeOfUniquePairs(const std::deque<std::pair<T1, T2>> initial_deque)
+      : deque_(std::move(initial_deque)),
+        set_(initial_deque.begin(), initial_deque.end()) {
+    if (set_.size() != initial_deque.size()) {
+      throw std::runtime_error("Duplicates detected!");
+    }
+  };
+
+  bool operator==(const DequeOfUniquePairs<T1, T2>& other) const {
+    return (deque_ == other.deque());
+  }
+
+  bool empty() const noexcept { return deque_.empty(); }
+
+  const std::deque<std::pair<T1, T2>> deque() const { return deque_; };
+
+  const std::unordered_set<std::pair<T1, T2>, pair_hash> set() const {
+    return set_;
+  };
+
+  bool insertFront(std::pair<T1, T2> element) {
+    if (set_.emplace(element).second) {
+      deque_.emplace_front(element);
+      return true;
+    }
+    return false;
+  };
+
+  std::size_t size() { return deque_.size(); };
+
+  std::pair<T1, T2> front() const { return deque_.front(); }
+
+  void popBack() {
+    auto last = deque_.back();
+    deque_.pop_back();
+    set_.erase(last);
+  };
+
+  bool has(std::pair<T1, T2> element) const {
+    return set_.find(element) != set_.end();
+  }
+
+  private:
+    std::deque<std::pair<T1, T2>> deque_;
+  std::unordered_set<std::pair<T1, T2>, pair_hash> set_;
+};
+
+}  // namespace uniquecontainers

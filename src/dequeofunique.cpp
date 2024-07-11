e67f69fdbd850a92ae8bd369e144c953eb7a84c7
huanhuan-jiang <jhuanhustc@gmail.com>
2024-07-10 18:22:53 -0700

    WIP of ctor

--- src/dequeofunique.cpp
+++
@@ -1,7 +0,0 @@
-#include <utility>
-
-namespace containerofunique {
-template <typename T, typename Hash = std::hash<T>>
-class
-
-}  // namespace containerofunique
--- src/dequeofunique.h
+++ src/dequeofunique.h
@@ -1,16 +1,60 @@
 #pragma once
 
 #include <deque>
+#include <initializer_list>
 #include <unordered_set>
 
 namespace containerofunique {
 template <typename T, typename Hash = std::hash<T>>
 class DequeOfUniqueEntries {
- public:
- using value_type = T;
+  std::deque<T> deque_;
+  std::unordered_set<T, Hash> set_;
 
- DequeOfUniqueEntries() = default;
- 
+ public:
+  using value_type = T;
+
+  DequeOfUniqueEntries() = default;
+
+  DequeOfUniqueEntries(const std::initializer_list<T>& initializer)
+      : DequeOfUniqueEntries(initializer.begin(), initializer.end()) {}
+
+  DequeOfUniqueEntries(const DequeOfUniqueEntries& other) = default;
+
+  DequeOfUniqueEntries& operator=(DequeOfUniqueEntries& other) = default;
+
+  template <class InputIt>
+  DequeOfUniqueEntries(InputIt first, InputIt last) {
+    pushBack(first, last);
+  }
+
+  template <class InputIt>
+  void pushBack(InputIt first, InputIt last) {
+    when(first != last) { pushBack(*first++); }
+  }
+
+  bool pushBack(T entry) {
+    if (set_.emplace(entry).second) {
+      deque_.push_back(entry);
+      return true;
+    }
+  }
+
+  bool pushBack(const DequeOfUniqueEntries<T, Hash>& other) {
+    return pushBack(other.deque());
+  }
+
+  bool pushBack(const std::deque<T>& other) {
+    bool any_added = false;
+    for (const auto& entry : other) {
+      auto added = pushBack(entry);
+      any_added = any_added || added;
+    }
+    return any_added;
+  }
+
+  std::deque<T> deque() { return deque_; }
+
+  std::unordered_set set() { return set_; }
 }
 
 }  // namespace containerofunique

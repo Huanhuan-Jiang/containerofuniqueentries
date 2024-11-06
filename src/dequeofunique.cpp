7b75a286705084d11a67342f767a994cd5a74f12
huanhuanjiang <jhuanhustc@gmail.com>
2024-11-06 20:35:22 +0800

    Update .gitignore

--- src/dequeofunique.cpp
+++ src/dequeofunique.cpp
@@ -1,24 +1,18 @@
 // #pragma once
 
-#include <algorithm>  // For std::find
-#include <cassert>    // For std::assert
+#include <algorithm>  // For for_each
 #include <compare>    // For std::weak_ordering
 #include <deque>
 #include <functional>  // For std::hash
 #include <initializer_list>
 #include <iostream>
-#include <memory>       // For std::allocator
-#include <optional>     // For std::nullopt
-#include <ranges>       // For ranges
-#include <type_traits>  // For std::is_same
+#include <optional>  // For std::nullopt
 #include <unordered_set>
-#include <utility>
+#include <utility>  // For std::swap
 
 namespace containerofunique {
 
-template <class T, class Hash = std::hash<T>, class KeyEqual = std::equal_to<T>,
-          class Allocator = std::allocator<T>>
-
+template <class T, class Hash = std::hash<T>, class KeyEqual = std::equal_to<T>>
 class deque_of_unique {
  public:
   // *Member types
@@ -26,26 +20,15 @@ class deque_of_unique {
   using key_type = T;
   using hasher = Hash;
   using key_equal = KeyEqual;
-  using allocator_type = Allocator;
-
-  static_assert(std::is_same_v<typename allocator_type::value_type, value_type>,
-                "Allocator::value_type must be same type as value_type");
-
-  using AllocTraits = std::allocator_traits<Allocator>;
   using reference = value_type&;
   using const_reference = const value_type&;
-  using pointer = typename AllocTraits::pointer;
-  using size_type = typename AllocTraits::size_type;
-  using difference_type = typename AllocTraits::difference_type;
-
-  using deque_type = std::deque<T, Allocator>;
-  using UnorderedSetType = std::unordered_set<T, Hash, KeyEqual, Allocator>;
-
-  using iterator = typename deque_type::iterator;
-  using const_iterator = typename deque_type::const_iterator;
-  using reverse_iterator = typename deque_type::reverse_iterator;
-  using const_reverse_iterator = typename deque_type::const_reverse_iterator;
-  using _deque_reference = typename deque_type::reference;
+  using DequeType = std::deque<T>;
+  using UnorderedSetType = std::unordered_set<T, Hash, KeyEqual>;
+  using size_type = typename DequeType::size_type;
+  using iterator = typename DequeType::iterator;
+  using const_iterator = typename DequeType::const_iterator;
+  using reverse_iterator = typename DequeType::reverse_iterator;
+  using const_reverse_iterator = typename DequeType::const_reverse_iterator;
 
   // Member functions
   // Constructor
@@ -59,25 +42,21 @@ class deque_of_unique {
   deque_of_unique(const std::initializer_list<T>& init)
       : deque_of_unique(init.begin(), init.end()) {}
 
-  deque_of_unique(const deque_of_unique& other) = default;
+  deque_of_unique(const deque_of_unique& other) { _push_back(other); }
 
   deque_of_unique(deque_of_unique&& other) noexcept {
     std::swap(deque_, other.deque_);
     std::swap(set_, other.set_);
-  };
+  }
 
   deque_of_unique& operator=(const deque_of_unique& other) = default;
-  deque_of_unique& operator=(deque_of_unique&& other) noexcept {
-    std::swap(deque_, other.deque_);
-    std::swap(set_, other.set_);
-    return *this;
-  };
+  deque_of_unique& operator=(deque_of_unique&& other) noexcept = default;
   deque_of_unique& operator=(std::initializer_list<T> ilist) {
     deque_of_unique temp(ilist);
-    std::swap(deque_, ilist.deque_);
-    std::swap(set_, ilist.set_);
+    std::swap(deque_, temp.deque_);
+    std::swap(set_, temp.set_);
     return *this;
-  };
+  }
 
   // Element access
   const_reference at(size_type pos) const { return deque_.at(pos); }
@@ -122,39 +101,30 @@ class deque_of_unique {
       return std::make_pair(deque_.insert(pos, std::move(value)), true);
     }
     return std::make_pair(pos, false);
-  };
-
-  template <class input_it>
-  std::pair<const_iterator, bool> insert(const_iterator pos, input_it first,
-                                         input_it last) {
-    bool any_added = false;
-    int num_inserted = 0;
-    auto return_it = pos;
-    for (auto it = first; it != last; ++it) {
-      auto added = set_.insert(*it).second;
-      if (added) {
-        auto tmp_it = deque_.insert(pos, *it);
-        if (++num_inserted == 1) {
-          return_it = tmp_it;
-        }
-      }
-      any_added = any_added || added;
-    }
-    return (std::make_pair(return_it, any_added));
   }
 
-  std::pair<const_iterator, bool> insert(const_iterator pos,
-                                         std::initializer_list<T> ilist) {
-    return (insert(pos, ilist.begin(), ilist.end()));
+  template <class input_it>
+  const_iterator insert(const_iterator pos, input_it first, input_it last) {
+    auto return_it = pos;
+    for (auto it = first; it != last; ++it) {
+      if (set_.insert(*it).second) {
+        return_it = deque_.insert(pos, *it);
+      }
+    }
+    return return_it;
+  }
+
+  const_iterator insert(const_iterator pos, std::initializer_list<T> ilist) {
+    return insert(pos, ilist.begin(), ilist.end());
   }
 
   template <class... Args>
   std::pair<const_iterator, bool> emplace(const_iterator pos, Args&&... args) {
     if (set_.emplace(args...).second) {
-      return (std::make_pair(deque_.emplace(pos, std::forward<Args>(args)...),
-                             true));
+      return std::make_pair(deque_.emplace(pos, std::forward<Args>(args)...),
+                            true);
     }
-    return (std::make_pair(pos, false));
+    return std::make_pair(pos, false);
   }
 
   template <class... Args>
@@ -174,13 +144,13 @@ class deque_of_unique {
   }
 
   void pop_front() {
-    auto f = deque_.front();
+    const auto& f = deque_.front();
     deque_.pop_front();
     set_.erase(f);
   }
 
   void pop_back() {
-    auto f = deque_.back();
+    const auto& f = deque_.back();
     deque_.pop_back();
     set_.erase(f);
   }
@@ -242,28 +212,24 @@ class deque_of_unique {
   void swap(deque_of_unique& other) noexcept {
     deque_.swap(other.deque_);
     set_.swap(other.set_);
-  };
+  }
 
   // Capacity
-  bool empty() const noexcept { return deque_.empty(); };
+  bool empty() const noexcept { return deque_.empty(); }
 
-  size_type size() const noexcept { return (deque_.size()); }
+  size_type size() const noexcept { return deque_.size(); }
 
   // operators
   auto operator<=>(const deque_of_unique& other) const {
-    return (deque_ <=> other.deque_);
-  }
-
-  bool operator==(const deque_of_unique& other) const {
-    return (deque_ == other.deque_);
+    return deque_ <=> other.deque_;
   }
 
   // Destructor
   ~deque_of_unique() = default;
 
   // Get member variables
-  deque_type deque() { return deque_; }
-  UnorderedSetType set() { return set_; }
+  const DequeType& deque() const { return deque_; }
+  const UnorderedSetType& set() const { return set_; }
 
   void print() const {
     std::cout << "Print out the deque_of_unique.\n";
@@ -286,13 +252,14 @@ class deque_of_unique {
   }
 
  private:
-  deque_type deque_;
+  DequeType deque_;
   UnorderedSetType set_;
 };  // class deque_of_unique
-}  // namespace containerofunique
+};  // namespace containerofunique
 
 int main() noexcept {
   std::cout << "Test constructors and operator '=' using int:\n";
+  std::cout << "Test empty().\n";
   containerofunique::deque_of_unique<int> dq_int_empty;
   if (dq_int_empty.empty() == false) {
     std::cout << "dq_int_empty is empty.\n";
@@ -300,6 +267,7 @@ int main() noexcept {
     dq_int_empty.print();
   }
 
+  std::cout << "Test deque_of_unique(const std::initializer_list<T>& init).\n";
   containerofunique::deque_of_unique<int> dq_int_init1{1};
   std::cout << "Print dq_int_init1:\n";
   dq_int_init1.print();
@@ -312,34 +280,44 @@ int main() noexcept {
   std::cout << "Print dq_int_init3:\n";
   dq_int_init3.print();
 
+  std::cout << "Test deque_of_unique(const deque_of_unique& other).\n";
   containerofunique::deque_of_unique<int> dq_int_init4(dq_int_init3);
   std::cout << "Print dq_int_init4:\n";
   dq_int_init4.print();
 
+  std::cout << "Test deque_of_unique(const deque_of_unique&& other).\n";
   containerofunique::deque_of_unique<int> dq_int_init5(std::move(dq_int_init3));
   std::cout << "Print dq_int_init5:\n";
   dq_int_init5.print();
 
+  std::cout
+      << "Test deque_of_unique& operator=(const deque_of_unique& other).\n";
   containerofunique::deque_of_unique<int> dq_int_init6 = dq_int_init5;
   std::cout << "Print dq_int_init6:\n";
   dq_int_init6.print();
 
+  std::cout
+      << "Test deque_of_unique& operator=(const deque_of_unique&& other).\n";
   containerofunique::deque_of_unique<int> dq_int_init7 =
       std::move(dq_int_init5);
   std::cout << "Print dq_int_init7:\n";
   dq_int_init7.print();
 
+  std::cout << "Test deque_of_unique(input_it first, input_it last).\n";
   std::deque<int> deque_int = {1, 2, 3, 4, 5};
   containerofunique::deque_of_unique<int> dq_int_init8(deque_int.begin(),
                                                        deque_int.end());
   std::cout << "Print dq_int_init8:\n";
   dq_int_init8.print();
 
+  std::cout
+      << "Test deque_of_unique& operator=(std::initializer_list<T> ilist).\n";
   containerofunique::deque_of_unique<int> dq = {1, 2, 3, 4};
   std::cout << "Print dq:\n";
   dq.print();
 
   std::cout << "Test element access:\n";
+  std::cout << "Test at(), front(), operator(), and back():\n";
   std::cout << "The first element of dq_int1: " << dq.front() << ".\n";
   std::cout << "The second element of dq_int1: " << dq.at(1) << ".\n";
   std::cout << "The third element of dq_int1: " << dq[2] << ".\n";
@@ -347,6 +325,7 @@ int main() noexcept {
 
   std::cout << "\n";
   std::cout << "Test iterators using int:\n";
+  std::cout << "Test cbegin() and cend().\n";
   std::cout << "The first element of dq is: " << *dq.cbegin() << ".\n";
   auto it = dq.cbegin();
   std::cout << "The second element of dq is: " << *(++it) << ".\n";
@@ -357,6 +336,7 @@ int main() noexcept {
   std::cout << "The third element of dq is: " << *(--dq.cend()) << ".\n";
 
   std::cout << "\nTest reverse iterators using int: \n";
+  std::cout << "Test rbegin() and crend().\n";
   std::cout << "The first element of reversed dq is : " << *dq.crbegin()
             << ".\n";
   auto rit = dq.crbegin();
@@ -369,40 +349,52 @@ int main() noexcept {
             << ".\n";
 
   std::cout << "\nTest modifiers using int: \n";
+  std::cout << "\nTest clear().\n";
   dq.clear();
   std::cout << "Print dq after clear: \n";
   dq.print();
   std::cout << "\nTest push_front and push_back using int:\n";
+
+  std::cout << "\nTest push_back(const T& value) and push_back(T&& value).\n";
   dq.push_back(3);
   int int1 = 4;
   dq.push_back(std::move(int1));
+
+  std::cout << "\nTest push_front(const T& value) and push_front(T&& value).\n";
   dq.push_front(2);
   int int2 = 1;
-  dq.push_front(std::move(int2));
+  dq.push_front(std::move(int2));  // {1, 2, 3, 4}
   dq.print();
 
+  std::cout << "\nTest pop_front() and pop_back().\n";
   dq.clear();
   dq = containerofunique::deque_of_unique<int>({1, 2, 3, 4});
   dq.pop_front();
-  std::cout << "Print dq after pop_front: \n";
+  std::cout << "Print dq after pop_front: \n";  // {2, 3, 4}
   dq.print();
   dq.pop_back();
-  std::cout << "Print dq after pop_back: \n";
+  std::cout << "Print dq after pop_back: \n";  // {2, 3}
   dq.print();
 
+  std::cout << "\nTest clear(), erase(const_iterator pos), "
+               "erase(const_iterator first, const_iterator last) \n";
   dq.clear();
   dq = containerofunique::deque_of_unique<int>({1, 2, 3, 4});
   std::cout << "Print dq after erase one element:" << *dq.erase(dq.cbegin())
             << "\n";
-  dq.print();
+  dq.print();  // {2, 3, 4}
   dq.erase(dq.cbegin(), dq.cbegin() + 2);
   std::cout << "Print dq after erase a range of elements: \n";
-  dq.print();
+  dq.print();  // {4}
 
+  std::cout << "insert(const_iterator pos, const T& value), "
+               "insert(const_iterator pos, T&& value), insert(const_iterator "
+               "pos, input_it first, input_it last), and insert(const_iterator "
+               "pos, std::initializer_list<T> ilist).\n";
   int value = 1;
   if (dq.insert(dq.cbegin(), value).second) {
     std::cout << "Print dq after insert one lvalue 1: \n";
-    dq.print();
+    dq.print();  // {1, 4}
   }
 
   if (dq.insert(dq.cbegin(), value).second) {
@@ -413,33 +405,37 @@ int main() noexcept {
   int value2 = 2;
   if (dq.insert(dq.cbegin() + 1, std::move(value2)).second) {
     std::cout << "Print dq after insert one rvalue 2: \n";
-    dq.print();
+    dq.print();  // {1, 2, 4}
   }
 
   auto values = containerofunique::deque_of_unique<int>({1, 2, 3, 4});
   dq.clear();
-  if (dq.insert(dq.cbegin(), values.cbegin(), values.cbegin() + 2).second) {
+  if (dq.insert(dq.cbegin(), values.cbegin(), values.cbegin() + 2) !=
+      dq.cbegin()) {
     std::cout << "Print dq after insert muliple elements: \n";
-    dq.print();
+    dq.print();  // {1, 2}
   }
   values.clear();
   values = containerofunique::deque_of_unique<int>({2, 5, 7, 8});
-  if (dq.insert(dq.cbegin(), values.cbegin(), values.cbegin() + 3).second) {
+  if (dq.insert(dq.cbegin(), values.cbegin(), values.cbegin() + 3) !=
+      dq.cbegin()) {
     std::cout
         << "Print dq after insert muliple elements again: \n";  // no printing
     dq.print();
   }
-  if (dq.insert(dq.cbegin(), {1, 2, 3, 4}).second) {
+  if (dq.insert(dq.cbegin(), {1, 2, 3, 4}) != dq.cbegin()) {
     std::cout << "Print dq after insert an initilizer list: \n";
     dq.print();
   }
 
+  std::cout << "Test emplace(const_iterator pos, Args&&... args).\n";
   dq.clear();
   dq = containerofunique::deque_of_unique<int>({1, 2, 3, 4});
   dq.emplace(dq.cbegin() + 2, 2);
   std::cout << "Emplace with 2:\n";
   dq.print();
-  dq.emplace(dq.cbegin() + 3, 7);
+  int value3 = 7;
+  dq.emplace(dq.cbegin() + 3, std::move(value3));
   std::cout << "Emplace with 7:\n";
   dq.print();
 
@@ -452,15 +448,18 @@ int main() noexcept {
   std::cout << "Emplace_back with 7:\n";
   dq.print();
 
+  std::cout << "\nTest emplace_back(Args&&... args).\n";
   dq.clear();
   dq = containerofunique::deque_of_unique<int>({1, 2, 3, 4});
   dq.emplace_front(2);
   std::cout << "Emplace_front with 2:\n";
   dq.print();
-  dq.emplace_front(7);
+  int value4 = 7;
+  dq.emplace_front(std::move(value4));
   std::cout << "Emplace_front with 7:\n";
   dq.print();
 
+  std::cout << "\nTest swap(deque_of_unique& other).\n";
   containerofunique::deque_of_unique<int> dq1({1, 2, 3, 4});
   containerofunique::deque_of_unique<int> dq2({5, 6, 7, 8});
   std::cout << "Before swap:\n";
@@ -471,28 +470,38 @@ int main() noexcept {
   dq1.print();
   dq2.print();
 
+  std::cout << "\nTest size().\n";
+  std::cout << "The size of dq2 is: " << dq2.size() << "\n";  // should be 4
+
+  std::cout << "\nTest operator <=>.\n";
   dq1.clear();
   dq2.clear();
   dq1 = containerofunique::deque_of_unique<int>({1, 2, 3, 4});
   dq2 = containerofunique::deque_of_unique<int>({5, 6, 7, 8});
   containerofunique::deque_of_unique<int> dq3(dq1);
-  if (dq1 == dq3) {
-    std::cout << "dq1 is equal to dq3\n";
+  if ((dq1 <=> dq3) == std::strong_ordering::equal) {
+    std::cout << "dq1 is equal to dq3.\n";
+  } else {
+    std::cout << "dq1 is not equal to dq3.\n";
   }
-  if (dq1 == dq2) {
-    std::cout << "dq1 is not equal to dq2\n";
+
+  if ((dq1 <=> dq2) == std::strong_ordering::equal) {
+    std::cout << "dq1 is  equal to dq2.\n";
+  } else {
+    std::cout << "dq1 is not equal to dq2.\n";
   }
+
   if (dq1 < dq2) {
-    std::cout << "dq1 is less than dq2\n";
+    std::cout << "dq1 is less than dq2.\n";
   }
   if (dq1 <= dq2) {
-    std::cout << "dq1 is less than or equal to dq2\n";
+    std::cout << "dq1 is less than or equal to dq2.\n";
   }
   if (dq2 > dq1) {
-    std::cout << "dq2 is greater than dq1\n";
+    std::cout << "dq2 is greater than dq1.\n";
   }
   if (dq2 >= dq1) {
-    std::cout << "dq2 is greater than or equal to dq1\n";
+    std::cout << "dq2 is greater than or equal to dq1.\n";
   }
 
   if ((dq1 <=> dq2) == std::weak_ordering::less) {
@@ -505,5 +514,17 @@ int main() noexcept {
     std::cout << "dq1 is equivalent to dq3.\n";
   }
 
+  std::cout << "\nTest getting member variables:\n";
+  dq1.clear();
+  dq1 = containerofunique::deque_of_unique<int>({1, 2, 3, 4});
+  dq1.print();
+  std::cout << "Get deque of dq1 using deque(): ";
+  std::for_each(dq1.deque().begin(), dq1.deque().end(),
+                [](const auto& num) { std::cout << num << " "; });
+  std::cout << "\n";
+  std::cout << "Get set of dq1 using set(): ";
+  std::for_each(dq1.set().begin(), dq1.set().end(),
+                [](const auto& num) { std::cout << num << " "; });
+  std::cout << "\n";
   return 0;
 }

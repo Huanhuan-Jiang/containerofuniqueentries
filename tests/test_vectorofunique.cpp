d2a18ed8906edd1463b7e1c3c4ee752af299e5e1
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-12-19 22:09:45 +0800

    Test ConstructorWithInitializerListChecksVectorAndSet

--- tests/test_vectorofunique.cpp
+++ tests/test_vectorofunique.cpp
@@ -30,3 +30,24 @@ TEST(VectorOfUniqueTest, ConstructorInitializesFromIterators) {
   EXPECT_THAT(std::vector<int>(vou.set().begin(), vou.set().end()),
               ::testing::UnorderedElementsAreArray(expect_vec));
 }
+
+TEST(DequeOfUniqueTest, ConstructorWithInitializerListChecksVectorAndSet) {
+  containerofunique::vector_of_unique dou1 = {1};
+  containerofunique::vector_of_unique dou2 = {1, 2};
+  containerofunique::vector_of_unique dou3 = {1, 2, 3, 3};  // duplicate elements
+
+  std::vector<int> dq1 = {1};
+  std::vector<int> dq2 = {1, 2};
+  std::vector<int> dq3 = {1, 2, 3};
+
+  EXPECT_EQ(dou1.vector(), dq1);
+  EXPECT_EQ(dou2.vector(), dq2);
+  EXPECT_EQ(dou3.vector(), dq3);
+
+  EXPECT_THAT(std::vector<int>(dou1.set().begin(), dou1.set().end()),
+              ::testing::UnorderedElementsAreArray(dq1));
+  EXPECT_THAT(std::vector<int>(dou2.set().begin(), dou2.set().end()),
+              ::testing::UnorderedElementsAreArray(dq2));
+  EXPECT_THAT(std::vector<int>(dou3.set().begin(), dou3.set().end()),
+              ::testing::UnorderedElementsAreArray(dq3));
+}

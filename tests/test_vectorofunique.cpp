9bf0d21818fa3146a2bbb89c94f5463a743654ff
Huanhuan-Jiang <jhuanhustc@gmail.com>
2024-12-19 18:39:02 +0800

    WIP

---
+++ tests/test_vectorofunique.cpp
@@ -0,0 +1,23 @@
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <compare>
+#include <concepts>
+#include <vector>
+#include <stdexcept>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "vectorofunique.h"
+
+TEST(VectorOfUniqueTest, DefaultConstructor) {
+  containerofunique::vector_of_unique<int> dou;
+  std::vector<int> emptydq;
+  std::unordered_set<int> emptyset;
+
+  EXPECT_EQ(dou.vector(), emptydq);
+  EXPECT_THAT(dou.set(), ::testing::UnorderedElementsAreArray(emptyset));
+}
